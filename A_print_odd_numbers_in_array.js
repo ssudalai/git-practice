4a6bd28e722458ca14cb270704412b1de4604929
ssudalai <sudalai3192@gmail.com>
2024-03-10 00:05:20 +0530

    roadmapclass3

---
+++ A_print_odd_numbers_in_array.js
@@ -0,0 +1,15 @@
+var numbers=[1,2,3,4,5,6,7,8,9];
+
+var fnoddnumbers = function(arr){
+         for (var i=0; i<arr.length; i++){
+            if (arr[i]%2!==0){
+                  console.log(arr[i]);
+
+            }
+
+         }
+
+
+}
+
+fnoddnumbers(numbers);
