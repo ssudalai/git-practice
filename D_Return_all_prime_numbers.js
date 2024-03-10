7a1b0789c76a6bd8fb623301697395ea8fccb04a
ssudalai <sudalai3192@gmail.com>
2024-03-11 00:58:32 +0530

    prime

---
+++ D_Return_all_prime_numbers.js
@@ -0,0 +1,12 @@
+//anonymous function
+
+let B =[1,2,3,4,5,6,7,8,9]
+
+for(i=0; i<B.length;i++){
+
+    if (B[i]/B[i]==B[i] && B[i]/B[i]==1){
+        console.log(B[i] ,"is a prime number")
+    }else {
+        console.log("it is a composite number ")
+    }
+}
