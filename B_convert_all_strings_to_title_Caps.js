b3c4a0c52b512c8ab2a872a351ce6b9a921c65d8
ssudalai <sudalai3192@gmail.com>
2024-03-10 15:04:57 +0530

    IIFE

--- B_convert_all_strings_to_title_Caps.js
+++ B_convert_all_strings_to_title_Caps.js
@@ -1,35 +1,39 @@
 // anonymous function
-var A = ["hello","world","bharat","india","off"]
+let CD =["hello","world","this","is","sudalai","is","good","boy"]
 
-var uppercase = function (b){
+
+let fn = function (b){
 
     return b.charAt(0).toUpperCase() + b.slice(1);
 }
-var uppercaseA=[];
-for(i=0;i<A.length;i++){
-     uppercaseA.push(uppercase(A[i]));
+let UPcse =[]
+for (let m=0; m<CD.length;m++){
+      UPcse.push(fn(CD[m]));
 
 }
+console.log(UPcse)
 
-console.log(uppercaseA);
+
+
+
 
 
 // IIFE
 
 let B = ["powder","coffee","arrayay","newdelhi","mumbai"]
-let iife = (function(c){
+let iife = (function(){
 
+    return function(c) {
+        return c.charAt(0).toUpperCase() + c.slice(1);
+    }}
 
-     let supercase =[];
-     for(d=0;d<c.length;d++){
-        supercase.push(c[d].charAt(0).toUpperCase() + c[d].slice(1));
-           
-       }
-       console.log(supercase);
+)();
+
+let uppcase = [];
+for (let g=0; g<B.length; g++){
+    uppcase.push(iife(B[g]));
 }
-
-)(B);
-   
+   console.log(uppcase)
 
 
 
