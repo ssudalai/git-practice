2fd6b8669b05e0c60621ca4d488dd24772e7644c
ssudalai <sudalai3192@gmail.com>
2024-03-12 08:53:40 +0530

    iife prime number

--- D_Return_all_prime_numbers.js
+++ D_Return_all_prime_numbers.js
@@ -1,3 +1,5 @@
+//anonymous function
+
 let B = [1, 2, 3, 4, 5, 99, 7, 8, 9];
 
 let isPrime = function (M) {
@@ -17,3 +19,26 @@ let isPrime = function (M) {
 B.forEach((number) => {
     console.log(isPrime(number));
 });
+
+//IIFE Function
+(function() {
+    let B = [1, 2, 3, 4, 5, 99, 7, 8, 9];
+
+    let isPrime = function(M) {
+        if (M < 2) {
+            return `${M} is not a prime number`;
+        }
+
+        for (let i = 2; i * i <= M; i++) {
+            if (M % i === 0) {
+                return `${M} is not a prime number`;
+            }
+        }
+        return `${M} is a prime number`;
+    };
+
+    B.forEach((number) => {
+        console.log(isPrime(number));
+    });
+})();
+
